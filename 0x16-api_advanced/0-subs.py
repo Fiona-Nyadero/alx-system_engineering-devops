e66e0fe6372839a63b9595b1ce5bbbca261f9a53
Fiona-Nyadero <fionanyadero@gmail.com>
2024-02-15 19:33:57 +0000

    Task 0 correction

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,10 @@ def number_of_subscribers(subreddit):
     """
     Queries Reddit API to return no. of subreddit subscribers
     """
+    
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
+
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "My API Client"}
 
