e4d122f0fe889720bcdc65b55c8fcc107960f2cd
Fiona-Nyadero <fionanyadero@gmail.com>
2024-02-16 02:09:11 +0000

    Task2&3

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,54 @@
+#!/usr/bin/python3
+"""Queries the Reddit API and parses it for given keywords"""
+
+import json
+import requests
+
+
+def count_words(subreddit, word_list, after="", count=[]):
+    """Count appearance of keywords on hot articles' titles"""
+
+    if after == "":
+        count = [0] * len(word_list)
+
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    reqst = requests.get(url,
+                           params={'after': after},
+                           allow_redirects=False,
+                           headers={'user-agent': 'bhalut'})
+
+    if reqst.status_code == 200:
+        data = reqst.json()
+
+        for topic in (data['data']['children']):
+            for word in topic['data']['title'].split():
+                for keyword in range(len(word_list)):
+                    if word_list[keyword].lower() == word.lower():
+                        count[keyword] += 1
+
+        after = data['data']['after']
+        if after is None:
+            save = []
+            for keyword in range(len(word_list)):
+                for kw in range(keyword + 1, len(word_list)):
+                    if word_list[keyword].lower() == word_list[kw].lower():
+                        save.append(kw)
+                        count[keyword] += count[kw]
+
+            for keyword in range(len(word_list)):
+                for kw in range(keyword, len(word_list)):
+                    if (count[kw] > count[keyword] or
+                            (word_list[keyword] > word_list[kw] and
+                             count[kw] == count[keyword])):
+                        aux = count[keyword]
+                        count[keyword] = count[kw]
+                        count[kw] = aux
+                        aux = word_list[keyword]
+                        word_list[keyword] = word_list[kw]
+                        word_list[kw] = aux
+
+            for keyword in range(len(word_list)):
+                if (count[keyword] > 0) and keyword not in save:
+                    print("{}: {}".format(word_list[keyword].lower(), count[keyword]))
+        else:
+            count_words(subreddit, word_list, after, count)
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Fetches a list of hot articles for a subreddit
+"""
+import requests
+after = None
+
+
+def recurse(subreddit, hot_list=[]):
+    """Returns top ten post titles recursively"""
+    global after
+    header = {'User-Agent': 'api_advanced-project'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    param = {'after': after}
+    resp = requests.get(url, params=param, headers=header,
+                           allow_redirects=False)
+
+    if resp.status_code == 200:
+        after_data = resp.json().get("data").get("after")
+        if after_data is not None:
+            after = after_data
+            recurse(subreddit, hot_list)
+        hot_titles = resp.json().get("data").get("children")
+        for hot_title in hot_titles:
+            hot_list.append(hot_title.get("data").get("title"))
+        return hot_list
+    else:
+        return (None)
